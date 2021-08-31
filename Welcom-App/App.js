47935e41d57e71f19a2ce3d06b03c2c103bccf53
alvalle09 <phredvalle@gmail.com>
2021-08-30 23:00:38 -0500

    create dist folder for deploy

--- Welcom-App/App.js
+++ Welcom-App/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
-    // TODO - implement component contents
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            displayName: '',
+            name: ''
+        }
+    }
+
+    render() {
+        return (
+            <>
+            <h1>Welcome to the App!!</h1>
+            <p>Hi there, {this.state.displayName}</p>
+            <input value={this.state.name} onChange="this.handleChange" />
+            
+            </>
+        );
+    }
 }
 
 export default App;
