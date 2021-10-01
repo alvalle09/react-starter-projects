7c9f826faecf606bde5c64ed22771538cac2ae90
alvalle09 <phredvalle@gmail.com>
2021-09-30 23:39:55 -0500

    setup useEffect example

--- Hooks-App/UseEffectExample.js
+++ Hooks-App/UseEffectExample.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const  UseEffectExample = () => {
+    const apiBaseUrl = 'https://dog.ceo/api/breeds/image/random';
+    const [doImageUrls, setDogImageUrls] = useState([]);
+    const [loadPictures, setLoadPictures] = useState(false);
+
+    const loadDogPictures = () => {
+        const numToLoad = Math.random() * 10;
+        fetch(`${apiBaseUrl}${numToLoad}`)
+        .then(resp => resp.json())
+        .then(data => setDogImageUrls(data.message));
+    }
     
+useEffect(() => {
+    loadDogPictures();
+    }, []);
+
+    useEffect(() => {
+        if(loadDogPictures) {
+            loadDogPictures();
+            setLoadPictures(false)
+        }
+    }, [loadDogPictures]);
+
+
     return (
         <>
             <p>Hello</p>
